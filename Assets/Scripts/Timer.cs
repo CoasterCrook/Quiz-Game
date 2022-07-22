78d1256391a96192ec6aea99b971f410e8ab0b93
Garrett Crook <GRCrook03@icloud.com>
2022-07-22 15:20:22 -0400

    Initial commit

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    float timerValue;
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    
+    public bool loadNextQuestion = true;
+    public bool IsAnsweringQuestion = false;
+    public float fillFraction;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+    
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (IsAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                IsAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                IsAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+    }
+}
